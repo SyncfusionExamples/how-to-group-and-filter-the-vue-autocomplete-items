c974dbfd1cef76b0da402c7568d8818ac2a8376a
Arulraj A <arul.raj47@gmail.com>
2020-01-08 16:54:10 +0530

    Committed the example project.

---
+++ README.md
@@ -0,0 +1,40 @@
+# How to Group and Filter the Vue AutoComplete Items of Syncfusion
+
+Get a quick overview on how to group and filter items in Vue AutoComplete. You will learn how to group the Vue AutoComplete items as well as how to filter those items. You will also see different filter types in AutoComplete, how to apply those filters to items, and how to include diacritic characters.
+
+Documentation Links: 
+https://ej2.syncfusion.com/vue/documentation/auto-complete/grouping
+https://ej2.syncfusion.com/vue/documentation/auto-complete/filtering
+
+
+Demo Links: 
+https://ej2.syncfusion.com/vue/demos/#/material/auto-complete/grouping-icon.html
+https://ej2.syncfusion.com/vue/demos/#/material/auto-complete/diacritics-filtering.html
+
+
+## Project pre-requisites
+Make sure that you have the compatible versions of Node and Vue-Cli in your machine before starting to work on this project.
+
+## How to run this application?
+To run this application, you need to first clone the `how-to-group-and-filter-the-vue-auto-complete-items` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionExamples/how-to-group-and-filter-the-vue-auto-complete-items autocomplete-component
+cd autocomplete-component
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed Vue packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running on development server
+Run `npm run serve` command for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
+## Further help
+
+To get more help on the vue CLI use go check out the [Vue-Cli README](https://github.com/vuejs/vue-cli/blob/master/README.md).
---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+      <!-- <div style="margin:10% 25%; width: 250px">
+        <ejs-autocomplete :highlight="true" popupHeight="400px"
+        popupWidth="250px" :dataSource='dataItem' 
+        placeholder='Select a value' :allowFiltering='true'
+        :ignoreAccent='true' autofill='true'>
+        </ejs-autocomplete >
+      </div> -->
+      <div style="margin:10% 25%; width: 250px">
+      <ejs-autocomplete :highlight="true" popupHeight="400px" 
+      popupWidth="250px" :dataSource='dataItem' 
+      :fields='dataFields' placeholder='Select a vegetable' 
+      :allowFiltering='true' :ignoreCase='false' 
+      filterType='Contains' >
+      </ejs-autocomplete >
+      </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { AutoCompletePlugin  } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(AutoCompletePlugin );
+
+export default Vue.extend({
+  data: function() {
+    return {
+      dataItem: [
+        { Name: 'Cabbage', Category: 'Leafy and Salad', Id: 'item1' },
+        { Name: 'Spinach', Category: 'Leafy and Salad', Id: 'item2' },
+        { Name: 'Wheat grass', Category: 'Leafy and Salad', Id: 'item3' },
+        { Name: 'Yarrow', Category: 'Leafy and Salad', Id: 'item4' },
+        { Name: 'Pumpkins', Category: 'Leafy and Salad', Id: 'item5' },
+        { Name: 'Chickpea', Category: 'Beans', Id: 'item6' },
+        { Name: 'Green bean', Category: 'Beans', Id: 'item7' },
+        { Name: 'Horse gram', Category: 'Beans', Id: 'item8' }
+        ],
+      dataFields: { 
+        groupBy: 'Category',  
+        value: 'Name' }
+		
+	 <!-- dataItem: [  
+            'Aeróbics',
+            'Aeróbics en Agua',
+            'Aerografía',
+            'Aeromodelaje',
+            'Águilas',
+            'Ajedrez',
+            'Ala Delta',
+            'Álbumes de Música',
+            'Alusivos',
+            'Análisis de Escritura a Mano'
+        ] -->
+
+    };
+  }
+});
+
+</script>
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+</style>
